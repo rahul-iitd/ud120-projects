f83111b3060f25458f5a1f09c1ca5c527fed2fa3
Rahul Bansal <Rahul.Bansal.cs116@cse.iitd.ac.in>
2018-02-27 14:54:32 +0530

    Update dt_author_id.py

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -9,6 +9,7 @@
 """
     
 import sys
+import numpy
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
@@ -20,12 +21,26 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+print len(features_test[0])
 
 #########################################################
 ### your code goes here ###
-
-
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+pred = clf.predict(features_test)
+print "Predicting time:", round(time()-t0, 3), "s"
+print clf.score(features_test, labels_test)
+y = numpy.bincount(pred)
+print y
 #########################################################
+
+
+
 
 
